c5bdd961b6b98ed8c17943566a387109df549dd1
Teddy Gunnar S <Teddy.Gunnar@sera.astra.co.id>
2025-06-18 14:07:51 +0700

    update php

--- update_email.blade.php
+++ update_email.blade.php
@@ -8,17 +8,29 @@
     <title>Login Info</title>
 </head>
 
-<body
-    style="margin: 0; padding: 0; background-color: #f5f5f5; display: flex; justify-content: center; font-family: Arial, Helvetica, sans-serif;">
-    <table width="100%" cellpadding="0" cellspacing="0" border="0" bgcolor="#f4f4f4">
-        <!-- HEADER -->
+<body style="
+      margin: 0;
+      padding: 0;
+      background-color: #f4f4f4;
+      font-family: 'Inter', sans-serif;
+    ">
+    <table width="100%" cellpadding="0" cellspacing="0" border="0" bgcolor="#f4f4f4"
+        style="width: 100%; background: #f4f4f4">
         <tr>
             <td align="center">
-                <table width="600" cellpadding="0" cellspacing="0" border="0" style="background: #fff; margin: 20px 0">
+                <table width="600" cellpadding="0" cellspacing="0" border="0"
+                    style="background: #fff; margin: 20px 0; width: 600px">
+                    <!-- HEADER -->
                     <tr>
                         <td>
-                            <img align="center" width="100%" src="https://teddygunnars.github.io/header_master.png"
-                                alt="header_master.png" />
+                            <img border="0" width="100%"
+                                src="https://tracomni.blob.core.windows.net/logo-trac/header_master.png"
+                                alt="TRACtoGo Header" style="
+                    display: block;
+                    border: 0;
+                    outline: none;
+                    text-decoration: none;
+                  " />
                         </td>
                     </tr>
                     <!-- END OF HEADER -->
@@ -42,22 +54,32 @@
                     </tr>
                     <!-- END OF BODY -->
                     <!-- FOOTER -->
-                    <tr style="background: #fbfbfb">
-                        <td style="text-align: center; font-size: 14px; padding: 20px">
+                    <tr>
+                        <td style="
+                    background: #fbfbfb;
+                    text-align: center;
+                    font-size: 14px;
+                    padding: 20px;
+                  ">
                             Email ini dikirim otomatis. Punya pertanyaan? Silahkan hubungi
                             kami.
                         </td>
                     </tr>
-                    <tr style="background: #fbfbfb">
-                        <td width="100%">
-                            <table cellpadding="0" cellspacing="0" align="center">
+                    <tr>
+                        <td style="background: #fbfbfb">
+                            <table role="presentation" cellpadding="0" cellspacing="0" width="100%">
                                 <tr>
-                                    <td style="padding-right: 10px">
-                                        <img src="https://teddygunnars.github.io/phone_icons.svg" width="18"
-                                            alt="phone.png" style="display: block; text-align: center" />
+                                    <td style="width: 150px; padding: 0 8px 0 0" align="right">
+                                        <img src="https://tracomni.blob.core.windows.net/logo-trac/phone_icons.svg"
+                                            width="18" height="18" border="0" alt="Telepon" style="
+                            outline: none;
+                            text-decoration: none;
+
+                            display: block;
+                          " />
                                     </td>
-                                    <td>
-                                        <a target="_blank" href="https://www.trac.astra.co.id/support/hubungi-kami"
+                                    <td align="left" style="width: 100px">
+                                        <a href="https://www.trac.astra.co.id/support/hubungi-kami" target="_blank"
                                             style="
                             color: #1a73e8;
                             text-decoration: none;
@@ -65,12 +87,16 @@
                             font-size: 14px;
                           ">1500-009</a>
                                     </td>
-                                    <td style="padding-left: 20px; padding-right: 10px">
-                                        <img width="18" src="https://teddygunnars.github.io/mail_icons.svg"
-                                            alt="email.png" style="display: block; text-align: center" />
+                                    <td style="width: 18px; padding: 0 8px 0 0" align="left">
+                                        <img src="https://tracomni.blob.core.windows.net/logo-trac/mail_icons.svg"
+                                            width="18" height="18" border="0" alt="Email" style="
+                            outline: none;
+                            text-decoration: none;
+                            display: block;
+                          " />
                                     </td>
                                     <td>
-                                        <a href="mailto:trac-astrarentcar@ai.astra.co.id" style="
+                                        <a href="mailto:trac-astrarentcar@ai.astra.co.id" target="_blank" style="
                             color: #1a73e8;
                             text-decoration: none;
                             font-weight: 500;
@@ -82,105 +108,114 @@
                         </td>
                     </tr>
                     <tr style="background: #fbfbfb">
-                        <td width="100%">
-                            <table cellpadding="0" cellspacing="0" align="center"
-                                style="padding-top: 20px; padding-bottom: 5px">
-                                <tr style="border-bottom: 1px solid black">
-                                    <td style="padding-right: 10px; padding-left: 10px">
-                                        <a href="https://www.instagram.com/trac_astra" target="_blank">
-                                            <img width="20" src="https://teddygunnars.github.io/ig_icons.svg"
+                        <td align="center">
+                            <table role="presentation" cellpadding="10" cellspacing="0" style="padding: 10px 0 0 0">
+                                <tr>
+                                    <td align="center">
+                                        <a href="https://www.instagram.com/trac_astra" target="_blank"
+                                            style="display: inline-block; padding: 0 10px">
+                                            <img width="20"
+                                                src="https://tracomni.blob.core.windows.net/logo-trac/ig_icons.svg"
                                                 alt="ig.png" style="display: block; text-align: center" />
                                         </a>
-                                    </td>
-                                    <td style="padding-right: 10px; padding-left: 10px">
-                                        <a href="https://www.tiktok.com/@trac_astra" target="_blank">
-                                            <img src="https://teddygunnars.github.io/ttk_icons.svg" width="20"
-                                                alt="ttk.png" style="display: block; text-align: center" />
+                                        <a href="https://www.tiktok.com/@trac_astra" target="_blank"
+                                            style="display: inline-block; padding: 0 10px">
+                                            <img src="https://tracomni.blob.core.windows.net/logo-trac/ttk_icons.svg"
+                                                width="20" alt="ttk.png" style="display: block; text-align: center" />
                                         </a>
-                                    </td>
-                                    <td style="padding-right: 10px; padding-left: 10px">
-                                        <a href="https://www.facebook.com/TRACAstraRentACar/" target="_blank">
-                                            <img width="20" src="https://teddygunnars.github.io/fb_icons.svg"
+                                        <a href="https://www.facebook.com/TRACAstraRentACar/" target="_blank"
+                                            style="display: inline-block; padding: 0 10px">
+                                            <img width="20"
+                                                src="https://tracomni.blob.core.windows.net/logo-trac/fb_icons.svg"
                                                 alt="fb.png" style="display: block; text-align: center" />
                                         </a>
-                                    </td>
-                                    <td style="padding-right: 10px; padding-left: 10px">
-                                        <a href="https://x.com/trac_astra" target="_blank">
-                                            <img width="20" src="https://teddygunnars.github.io/x_icons.svg" alt="x.png"
-                                                style="display: block; text-align: center" />
+                                        <a href="https://x.com/trac_astra" target="_blank"
+                                            style="display: inline-block; padding: 0 10px">
+                                            <img width="20"
+                                                src="https://tracomni.blob.core.windows.net/logo-trac/x_icons.svg"
+                                                alt="x.png" style="display: block; text-align: center" />
                                         </a>
-                                    </td>
-                                    <td style="padding-right: 10px; padding-left: 10px">
                                         <a href="https://www.youtube.com/channel/UC-9jLt-k0R30YzImXxnn6PQ"
-                                            target="_blank">
-                                            <img width="20" src="https://teddygunnars.github.io/yt_icons.svg"
+                                            target="_blank" style="display: inline-block; padding: 0 10px">
+                                            <img width="20"
+                                                src="https://tracomni.blob.core.windows.net/logo-trac/yt_icons.svg"
                                                 alt="yt.png" style="display: block; text-align: center" />
                                         </a>
                                     </td>
                                 </tr>
                             </table>
-                    <tr style="background: #fbfbfb">
-                        <td align="center">
-                            <hr style="
-                          border: 0;
-                          border-top: 2px solid #e0e0e0;
-                          width: 550px;
-                        " />
-                        </td>
-                    </tr>
-            </td>
-            <td style="background: #fbfbfb">
-                <table width="600" cellpadding="0" cellspacing="0" border="0s" style="
-                      background: #fbfbfb;
-                      margin-top: 5px;
-                      padding-right: 20px;
-                    ">
-                    <tr style="background: #fbfbfb">
-                        <td colspan="2" width="100%" align="right" style="font-size: 14px; padding-right: 76px">
-                            Unduh aplikasi TRACtoGo di:
                         </td>
                     </tr>
                     <tr>
-                        <td align="left" style="padding-left: 24px; padding-bottom: 25px">
-                            <img src="https://teddygunnars.github.io/brand_logo_footer.svg" alt="brand_logo2.jpg"
-                                width="100" style="display: block" />
-                        </td>
-                        <td align="right">
-                            <table cellpadding="0" cellspacing="0">
+                        <td align="center" style="background: #fbfbfb">
+                            <table width="100%" align="center" cellpadding="0" cellspacing="0"
+                                style="margin: 10px auto 0 auto">
                                 <tr>
-                                    <td>
-                                        <a href="https://apps.apple.com/us/app/tractogo-rental-mobil-bus/id1459840738"
-                                            target="_blank">
-                                            <img src="https://teddygunnars.github.io/download_apple.svg"
-                                                alt="download_apple.jpg" width="120" style="
-                                    display: block;
-                                    margin: 0 10px;
-                                    padding-bottom: 15px;
-                                  " />
-                                        </a>
+                                    <td width="35">&nbsp;</td>
+                                    <td style="
+                          border-top: 2px solid #e0e0e0;
+                          line-height: 1px;
+                          height: 1px;
+                        ">
+                                        &nbsp;
+                                    </td>
+                                    <td width="35">&nbsp;</td>
+                                </tr>
+                            </table>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td align="right" style="padding: 0 107px 0; background: #fbfbfb; font-size: 14px">
+                            Unduh aplikasi TRACtoGo di:
+                        </td>
+                    </tr>
+                    <tr style="background: #fbfbfb">
+                        <td>
+                            <table role="presentation" cellpadding="10" cellspacing="0" width="100%">
+                                <tr>
+                                    <td align="center">
+                                        <img border="0"
+                                            src="https://tracomni.blob.core.windows.net/logo-trac/brand_logo_footer.svg"
+                                            alt="brand_logo2.jpg" width="85" style="display: block" />
                                     </td>
                                     <td>
-                                        <a href="https://play.google.com/store/apps/details?id=com.trac.tractogo&pcampaignid=web_share"
-                                            target="_blank">
-                                            <img src="https://teddygunnars.github.io/download_play.svg"
-                                                alt="download_play.jpg" width="120" style="
+                                        <table role="presentation" cellpadding="0" cellspacing="0" width="100%">
+                                            <tr>
+                                                <td align="right">
+                                                    <a href="https://apps.apple.com/us/app/tractogo-rental-mobil-bus/id1459840738"
+                                                        target="_blank">
+                                                        <img border="0"
+                                                            src="https://tracomni.blob.core.windows.net/logo-trac/download_apple.svg"
+                                                            alt="download_apple.jpg" width="120" style="
+                                    display: block;
+                                    margin: 0 10px;
+                                    padding: 0 0 15px 0;
+                                  " />
+                                                    </a>
+                                                </td>
+                                                <td align="left" width="150">
+                                                    <a href="https://play.google.com/store/apps/details?id=com.trac.tractogo&pcampaignid=web_share"
+                                                        target="_blank">
+                                                        <img border="0"
+                                                            src="https://tracomni.blob.core.windows.net/logo-trac/download_play.svg"
+                                                            alt="download_play.jpg" width="120" style="
                                     display: block;
                                     margin: 0 5px;
-                                    padding-bottom: 15px;
+                                    padding: 0 0 15px 0;
                                   " />
-                                        </a>
+                                                    </a>
+                                                </td>
+                                            </tr>
+                                        </table>
                                     </td>
                                 </tr>
                             </table>
                         </td>
                     </tr>
+                    <!-- END OF FOOTER -->
                 </table>
             </td>
         </tr>
-    </table>
-    </td>
-    </tr>
-    <!-- END OF FOOTER -->
     </table>
 </body>
 
